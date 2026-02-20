d2443d74aad0b4cb5635f1c94cf1b166a5696f59
Evan <88143635+evan052006@users.noreply.github.com>
2026-02-20 09:34:35 +0700

    fix: added sonarcloud dependency for gradle

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "id.ac.ui.cs.advprog"
