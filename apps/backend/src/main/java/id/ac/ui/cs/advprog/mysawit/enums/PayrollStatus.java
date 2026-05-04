96fd3b9c3de03833e3af21ffbffe35469e17d38e
Reyhan Ardian <reyhanardian16@gmail.com>
2026-05-04 12:29:19 +0700

    feat: added payroll model

---
+++ apps/backend/src/main/java/id/ac/ui/cs/advprog/mysawit/enums/PayrollStatus.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.mysawit.enums;
+
+public enum PayrollStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
---
+++ apps/backend/src/main/java/id/ac/ui/cs/advprog/mysawit/model/Payroll.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.mysawit.model;
+
+import id.ac.ui.cs.advprog.mysawit.enums.PayrollStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payroll")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payroll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PayrollStatus status;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+        if (status == null) {
+            status = PayrollStatus.PENDING;
+        }
+    }
+}
